4d967c353d1076f75468282148f8f22d6b1ee6f7
BRIAN SANDON (RIT Student) <bxs9775@ad.rit.edu>
2019-02-08 09:53:48 -0500

    barChart v2 - broken

--- main.js
+++ main.js
@@ -7,6 +7,68 @@ function rowConverter(row) {
   }
 }
 
+let chartTypes = {
+  barChart: (dataset, getDataX, getDataY) => {
+    let display = function(chart){
+      console.dir(this);
+      
+      this.dataset.sort((a,b) => getDataX(b) - getDataX(a));
+      
+      chart.attr('width', this.width)
+        .attr('height', this.height);
+      
+      // our range is limited from 0 to width - 100, 
+      // which is for the 80 pixels on left for axis and 
+      // 20 pixels on right for padding
+      let xScale = d3.scaleLinear()
+      .domain([this.xMin,this.xMax])
+      .rangeRound([0, this.width - this.xPadding]);
+      
+      // using scale band to work with nominal values 
+      // the Array.map() call allows us to get a new array
+      // by calling a function on each item of the source array 
+      // here it pulls out the app_name
+      let yScale = d3.scaleBand()
+      .domain(this.dataset.map(this.getDataY))
+      .rangeRound([this.yPadding, this.height - this.yPadding]);
+      
+      chart.selectAll('rect')
+        .data(dataset)
+        .enter()
+        .append('rect')
+        .attr('x', 80)
+        .attr('y', (d) => yScale(d.app_name))
+        .attr('width', (d) => xScale(this.getDataX(d)))
+        .attr('height', this.barHeight)
+        .attr('fill', this.color);
+      
+      // AXES
+      chart.append('g')
+        .attr('class', 'xaxis axis')
+        .attr('transform', `translate(80, ${this.height - 20})`)
+        .call(d3.axisBottom(xScale));
+      
+      chart.append('g')
+        .attr('class', 'yaxis axis')
+        .attr('transform', `translate(80,0 )`)
+        .call(d3.axisLeft(yScale));
+    };
+    return {
+      dataset: dataset,
+      width: 1200,
+      height: dataset.length * 30,
+      yPadding: 22,
+      XPadding: 100,
+      barHeight: 18,
+      color: "blue",
+      getDataX: getDataX,
+      getDataY: getDataY,
+      xMin: 0,
+      xMax: d3.max(dataset,getDataX),
+      display: display,
+    };
+  }
+};
 
 function makeBarChart(chart, dataset, color, getDataX, getDataY, xMin, xMax){
   
@@ -67,10 +129,10 @@ function makeScatterplot(){
 function makeChart1(dataset) {
   let chart1 = d3.select('#chart1');
   
-  let xMin = 0;
-  let xMax = d3.max(dataset, (d) => d.downloads);
-  
-  makeBarChart(chart1,dataset,'cornflowerblue',(d) => d.downloads,(d) => d.app_name,xMin,xMax);
+  let barChart1 = chartTypes.barChart(dataset,(d) => d.downloads,(d) => d.app_name);
+  barChart1.color = "cornflowerblue";
+  barChart1.display = barChart1.display.bind(barChart1);
+  barChart1.display(chart1);
 }
 
 function makeChart2(dataset) {
